d999cf0e9b35e96ed86217de83b19f30debb4785
Nestor20193767 <74310266+Nestor20193767@users.noreply.github.com>
2024-10-19 15:54:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ page = st.sidebar.radio("Seleccione una página:", ["Registrar Placa", "Buscar P
 if page == "Registrar Placa":
     # Registro de nueva placa
     st.subheader("Registrar Nueva Placa")
+    st.write(f'Esta es la contraseña: {st.scretes["password"]}')
     placa = st.text_input("Placa del Vehículo:")
     nombre = st.text_input("Nombre del Dueño:")
     tipo = st.selectbox("Tipo de Vehículo:", ["Policía", "Ejército", "Fuerza Aérea", "Naval"])
