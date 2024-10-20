11e7c13efa7ee9e6039cd12b92e453abd1ff7c68
Nestor20193767 <74310266+Nestor20193767@users.noreply.github.com>
2024-10-20 13:59:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main_page():
         </style>
         <div class="header">
             <img src="https://raw.githubusercontent.com/Nestor20193767/Ejercito/main/PLA___2_-removebg-preview%20(1).png" width="70">
-            <h1 style="margin: 0;">"SIREVE: Sistema de Registro de Placas de Vehículos"</h1>
+            <h1 style="margin: 0;">SIREVE: Sistema de Registro de Placas de Vehículos</h1>
         </div>
         """,
         unsafe_allow_html=True
