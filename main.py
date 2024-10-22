dab02d498011fd0eff7765f4724af82383d819e3
Nestor20193767 <74310266+Nestor20193767@users.noreply.github.com>
2024-10-22 12:29:42 -0500

    Update main.py - TITULO
    El titulo a cambiado

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def main_page():
         </style>
         <div class="header">
             <img src="https://raw.githubusercontent.com/Nestor20193767/Ejercito/main/PLA___2_-removebg-preview%20(1).png" width="80">
-            <h1 style="margin: 0;">SIREVE</h1>
+            <h1 style="margin: 0;">CAUSA</h1>
         </div>
         """, unsafe_allow_html=True
     ) 
